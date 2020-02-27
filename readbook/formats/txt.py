4a08738e543948e7cb7c9716639582fc022d4835
Xiaoyang Liu <admin@siujoeng-lau.com>
2020-02-27 22:26:49 +0800

    Add detailed info for books in 'txt' format.
    Signed-off-by: Siujoeng Lau <admin@siujoeng-lau.com>

--- readbook/formats/txt.py
+++ readbook/formats/txt.py
@@ -15,7 +15,10 @@ class TxtReader(BaseReader):
             print('[Error] Cannot open', self.file_name)
 
     def get_book_info(self):
-        book_info = {'name': self.file_name, 'type': 'txt', 'file_size': ''}
+        book_info = {'name': self.file_name, 'type': 'txt', 'file_size': '',
+                     'page_numbers': self.txt_book.get_page_numbers(),
+                     'word_counts': self.txt_book.get_word_counts(),
+                     'read_time': self.txt_book.get_read_time()}
         return book_info
 
 
@@ -28,16 +31,35 @@ class TxtBook(BaseBook):
     def __init__(self, file_handler):
         super().__init__()
         self.file_handler = file_handler
+        self.plain_list = self.get_plain_text(as_list=True)
+        self.word_counts = -1
 
     def get_plain_text(self, as_list=False):
         if as_list:
-            return self.file_handler.readlines()
+            if not self.plain_list:
+                self.plain_list = self.file_handler.readlines()
+            return self.plain_list
         else:
             return self.file_handler.read()
 
     def get_line(self, line_number):
-        plain_list = self.get_plain_text(as_list=True)
-        return plain_list[line_number]
+        if line_number < len(self.plain_list):
+            return self.plain_list[line_number]
+        else:
+            return None
+
+    def get_word_counts(self):
+        if self.word_counts == -1:
+            for line in self.plain_list:
+                self.word_counts += len(line.split())
+            self.word_counts += 1
+        return self.word_counts
+
+    def get_read_time(self):
+        return self.get_word_counts() / 265
+
+    def get_page_numbers(self):
+        return len(self.plain_list) / 25
 
     def get_info(self):
         pass
